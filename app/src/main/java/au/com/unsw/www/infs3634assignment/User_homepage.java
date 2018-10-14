2951993576dc975050fec25343c80af185862eb7
Martin Zhang <z5114997@ad.unsw.edu.au>
2018-10-14 12:03:33 +1100

    Adjusted UI and Toolbar for specific activities

--- app/src/main/java/au/com/unsw/www/infs3634assignment/User_homepage.java
+++ app/src/main/java/au/com/unsw/www/infs3634assignment/User_homepage.java
@@ -1,13 +1,38 @@
 package au.com.unsw.www.infs3634assignment;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 public class User_homepage extends AppCompatActivity {
-
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_homepage);
+
+        mDrawerLayout = findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open,R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        android.support.v7.widget.Toolbar toolbar =findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
